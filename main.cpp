32eb461f20eb5b96839803a5c87355ba2a17ed38
highwhite <kamosannoouchi@gmail.com>
2012-02-24 20:24:07 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <exception>
-#include "prime.hpp"
+#include <exception>
+#include <vector>
+#include <list>
 
+class prime{
+	int MIN;
+	int MAX;
+	public:
+void run(){
+	MIN=1;
+	MAX=20;
+	std::vector<int> i_base;
+	std::vector<int> i_list;
+	for(;MIN<MAX;)
+		i_base.push_back(MIN++);
+	i_list.push_back(*i_base.begin());
+	std::cout<<*i_list.begin()<<std::endl;
+};
+};
 int main() try {
- cpp_morijobi::prime::prime<>().run();
+	prime ho;
 }catch(const std::exception& e){
-std::cerr << e.what();
+	std::cerr << e.what();
 }
