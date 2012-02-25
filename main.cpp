d2bec30660b0198c694879b3d932a79574a58ecf
highwhite <kamosannoouchi@gmail.com>
2012-02-25 23:20:44 +0900

    kono erase muzui...

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <exception>
+#include <algorithm>
 #include <vector>
 #include <list>
 class prime{
@@ -12,20 +13,25 @@ class prime{
 	public:
 void run(){
 	sum=1;
-	MIN=1;
+	MIN=2;
 	MAX=20;
 	std::vector<int> i_base;
 	std::vector<int> i_list;
 	for(;MIN<MAX;)
 		i_base.push_back(MIN++);
-	for(ite_base=i_base.begin(),position=0;(*ite_base) != MAX; ite_base++,position++ ){
-	sum++;
+	for(ite_base=i_base.begin(),ite_list=i_list.begin();i_base.end() != ite_base; ite_base++,ite_list++){
+	std::cout << *ite_base<< std::endl;
 	i_list.push_back(*ite_base);
 	i_base.erase(ite_base);
-	for_each(ite_base, i_base.end(), [i_list,sum](int x) -> int{if(x==i_list.at(position)*sum)i_base.erase(&x); });
+
+	ite_list=i_list.end();
+	
+
+	////for_each(ite_base, i_base.end(), [i_list,sum](int x) -> int{if(x==i_list.at(position)*sum)i_base.erase(&x); });
 
 	}
-	std::cout<<*i_list.begin()<<std::endl;
+	std::cout<<"終わったよ!!";
+		
 };
 };
 int main() try {
