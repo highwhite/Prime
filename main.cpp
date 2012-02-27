816b105004a744de76c7b64e10998f701423d708
highwhite <kamosannoouchi@gmail.com>
2012-02-27 22:31:01 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,53 +12,58 @@ class prime{
 	std::vector<int>::iterator ite_list;
 	public:
 void run(){
+
+	std::cout << "素数計算プログラム開始"<<std::endl<<std::endl;
+	std::cout << "18~32までの数字を入力してください!!2からそこまでの素数を計算します!!"<<std::endl;
+	std::cout << ">"; std::cin >> MAX;
+
 	position=0;
-	sum=2;
 	MIN=2;
-	MAX=20;
+	sum=2;
 	std::vector<int> i_base;
 	std::vector<int> i_list;
+
 	for(;MIN<MAX;)
 		i_base.push_back(MIN++);
-	std::cout<<"始まったよ!!";
 
 	ite_base=i_base.begin();
 	i_list.push_back(*ite_base);
 	i_base.erase(ite_base);
 
 	for(ite_list=i_list.begin();(*ite_list)*(*ite_list) <= *(i_base.end()-1); ite_list++,position++){
-	std::cout << " ite_list no value1 "<<*ite_list<< std::endl;
-	std::cout << " i_list push_back"<<*ite_base<< std::endl;
-	std::cout << " ite_list no value2 "<<*ite_list<< std::endl;
-	i_list.push_back(*ite_base);
-	std::cout << "i_base erase"<< *ite_base<< std::endl;
-	i_base.erase(ite_base);
-	
-	ite_list=i_list.begin()+position;
-
-	for(ite_base=i_base.begin();i_base.end() != ite_base; ite_base++){
-		std::cout <<"*ite_base="<< *ite_base<< std::endl;
-		std::cout <<"*ite_list="<< *ite_list<< std::endl;
-	if(*ite_list*sum>=*ite_base){
-		if(*ite_list*sum==*ite_base){
-		std::cout << "inside i_base erase"<< *ite_base<< std::endl;
+		std::cout << " i_list push_back"<<*ite_base<< std::endl;
+		i_list.push_back(*ite_base);
+		std::cout << "i_base erase"<< *ite_base<< std::endl;
 		i_base.erase(ite_base);
-		}
-		sum++;
-	}
 	
+		ite_list=i_list.begin()+position;
+
+		for(ite_base=i_base.begin();i_base.end() != ite_base; ite_base++){
+			std::cout <<"*ite_base="<< *ite_base<< std::endl;
+			std::cout <<"*ite_list="<< *ite_list<< std::endl;
+			std::cout <<"sum value"<< sum<<std::endl;
+			if(*ite_list*sum<=*ite_base){
+				if(*ite_list*sum==*ite_base){
+					std::cout << "inside i_base erase"<< *ite_base<< std::endl;
+					i_base.erase(ite_base);
+				}
+			sum++;
+			}
+		}
+		sum=2;
+		ite_base=i_base.begin();
 	}
-	sum=2;
-	ite_base=i_base.begin();
+	std::cout << "素数は以下の通りです！" <<std::endl
+	std::cout << "i_listのリストは";
+	for(ite_base=i_base.begin(),ite_list=i_list.begin();i_list.end() != ite_list; ite_list++){
+		std::cout << *ite_list<<" ";
 	}
-	std::cout << "i_baseのリストは";
+	std::cout << std::endl<<"i_baseのリストは";
 	for(ite_base=i_base.begin(),ite_list=i_list.begin();i_base.end() != ite_base; ite_base++){
 		std::cout << *ite_base<<" ";
 	}
-	//std::cout << *ite_list<< std::endl;
 
 	std::cout<<"です。終わったよ!!";
-	//for(ite_base=i_base.begin(),ite_list=i_list.begin();i_base.end() != ite_base; ite_list++)
 
 };
 };
